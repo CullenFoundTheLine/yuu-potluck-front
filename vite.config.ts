3afa509174cac030d3088c5dbb8b5ba24b5335aa
Connor <34824423+EasterLee@users.noreply.github.com>
2026-06-05 10:47:02 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	// Optional: Silence Sass deprecation warnings. See note below.
+	css: {
+		preprocessorOptions: {
+			scss: {
+				silenceDeprecations: ["import", "color-functions", "global-builtin", "if-function"],
+			},
+		},
+	},
+});
